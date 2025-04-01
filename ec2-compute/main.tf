7949ab0af64b7640b5b72f44a9dd25231f3bd5d5
Ranjan Dalai <ranjandalai4049@gmail.com>
2025-04-01 16:14:23 +0530

    Update main.tf

--- ec2-compute/main.tf
+++ ec2-compute/main.tf
@@ -5,5 +5,5 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = "ami-0866a3c8686eaeeba" # Specify an appropriate AMI ID
   instance_type = "t2.micro"
-  subnet_id     = "subnet-0b24c237f30650250"
+  subnet_id     = "subnet-0d4e2e25864d04150"
 }
